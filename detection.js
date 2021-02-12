7c182c4b2765603296727d746c5e47e84d31837f
ft559 <56762657+ft559@users.noreply.github.com>
2021-02-12 13:24:52 +0800

    Update detection.js

--- detection.js
+++ detection.js
@@ -1,6 +1,7 @@
 
 if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-     window.location.href="https://ft559.github.io/mobile.html"
+     document.write("<h1>这是移动端浏览器吧（狗头）</h1>")
 }else{
-     window.location.href="https://ft559.github.io/pc.html"
+     document.write("<h1>这是pc端浏览器吧（狗头）</h1>")
+     
 }
