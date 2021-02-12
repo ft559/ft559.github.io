9fee7678193b1b550f5fa9c739ffed1ff31e625b
ft559 <56762657+ft559@users.noreply.github.com>
2021-02-12 12:53:23 +0800

    Create detection.js

---
+++ detection.js
@@ -0,0 +1,6 @@
+
+if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+     window.location.href="https://ft559.github.io/mobile.html"
+}else{
+     window.location.href="https://ft559.github.io/pc.html"
+}
